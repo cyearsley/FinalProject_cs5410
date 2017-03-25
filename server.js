ec520e702c3e0a9b85faa3397a2cbdd3df379474
Caleb Yearsley <cnyearsley@outlook.com>
2017-03-25 14:15:14 -0600

    Setting up environment.

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 console.log("Starting Application...");
+
+var express = require('express');
+var app = express();
+var port = 8000;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function (req, res) {
+	console.log("on home page");
+});
+
+app.get('/play', function (req, res) {
+	console.log("on play page");
+});
+
+app.listen(port, function (err) {
+	console.log("Running server on port: ", port);
+});
