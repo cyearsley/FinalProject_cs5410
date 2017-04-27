96bbec80a3608d83b7a132559130ed8c537eda4b
Caleb Yearsley <cnyearsley@outlook.com>
2017-04-26 22:31:43 -0600

    Feature:  worlds are now generated with trees.

--- src/util/server.createWorldArray.js
+++ src/util/server.createWorldArray.js
@@ -3,6 +3,53 @@ module.exports = function (size) {
 	let height = 1000;
 	let worldArr = [];
 
+	let trees = {
+		tree1: [
+						['e','l','e'],
+						['l','l','l'],
+						['l','l','l'],
+						['e','w','e'],
+						['e','w','e'],
+						['e','w','e'],
+		],
+
+		tree2: [
+						['e','e','e','l','e','e','e'],
+						['e','e','l','l','l','e','e'],
+						['e','e','l','w','l','e','e'],
+						['e','e','e','w','e','e','e'],
+						['e','l','e','w','e','e','e'],
+						['l','w','w','w','e','l','e'],
+						['e','l','e','w','l','w','l'],
+						['e','e','e','w','w','w','e'],
+						['e','e','e','w','e','e','e']
+		],
+
+		tree3: [
+						['e','l','e'],
+						['l','w','l'],
+						['e','w','e']
+		],
+
+		tree4: [
+						['e', 'l', 'e'],
+						['l', 'w', 'l'],
+						['l', 'w', 'l'],
+						['e', 'w', 'e'],
+						['e', 'w', 'e'],
+						['e', 'w', 'e'],
+						['e', 'w', 'e'],
+						['e', 'w', 'e'],
+						['e', 'w', 'e']
+		]
+	}
+
+	let map = {
+		 e: 'empty',
+		 l: 'leaves',
+		 w: 'wood'
+	}
+
 	for (let ii = 0; ii < 1000; ii += 1) {
 		worldArr.push([]);
 		for (let jj = 0; jj < 3000; jj += 1) {
@@ -19,9 +66,32 @@ module.exports = function (size) {
 				let randomVal = Math.floor(Math.random() * (6 - 1)) + 1;
 				let blockType = 'grass';
 				let backType = 'dirt';
-				if (randomVal <= 2) {
+				let ifTree = Math.floor(Math.random() * (10 - 1)) + 1;
+
+				if (randomVal <= 1) {
 					blockType = 'empty';
 					backType = blockType;
+				}
+				else {
+					if (ifTree >= 9) {
+						let treeIndex = (+Math.floor(Math.random() * (4 - 1)) + 1);
+						let treeType = trees['tree' + treeIndex];
+						if (ii - treeType.length > 0 && jj + treeType.length < 3000) {
+							let startII = ii - treeType.length;
+							let startJJ = jj - treeType[0].length;
+
+							for (yy = 0; yy < treeType.length; yy += 1) {
+								for (xx = 0; xx < treeType[0].length; xx += 1) {
+									if (typeof worldArr[startII+yy] !== 'undefined' && typeof worldArr[startII+yy][startJJ+xx] !== 'undefined') {
+										worldArr[startII+yy][startJJ+xx].blockType = map[treeType[yy][xx]];
+									}
+								}
+								if (yy === treeType.length - 1) {
+									worldArr[startII+treeType.length][startJJ+Math.floor(treeType[0].length/2)].blockType = 'grass';
+								}
+							}
+						}
+					}
 				}
 				worldArr[ii].push({
 					blockType: blockType,
